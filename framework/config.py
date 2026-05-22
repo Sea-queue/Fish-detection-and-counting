469204b80e3c824dc1d71f47c6970a0563477693
Alaska Tengli <tengli.a@northeastern.edu>
2026-05-22 09:52:15 -0400

    Added grayscale preprocessing option for bright/overexposed videos

--- framework/config.py
+++ framework/config.py
@@ -37,6 +37,7 @@ class RunConfig:
     count_conf: float = 0.70
     majority_ratio: float = 0.70
     min_track_len: int = 10
+    grayscale: bool = False
     no_display: bool = False
 
     # ------------------------------------------------------------------ #
@@ -89,6 +90,9 @@ class RunConfig:
             ),
             min_track_len=_pick(
                 getattr(args, "min_track_len", None), "min_track_len", 10
+            ),
+            grayscale=bool(
+                getattr(args, "grayscale", False) or dataset_entry.get("grayscale", False)
             ),
             no_display=getattr(args, "no_display", False),
         )
